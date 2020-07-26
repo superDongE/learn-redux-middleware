17d888c95a1308f8dfd8b54a9baaf0b44f0ef397
superDongE <dlagoehd13@gmail.com>
2020-07-27 02:37:59 +0900

    redux-middleware

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Counter from "../components/Counter";
+import { useSelector, useDispatch } from "react-redux";
+import { increase, decrease } from "../modules/counter";
+
+function CounterContainer() {
+  const number = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    //액션만들고 디스패치
+    dispatch(increase());
+  };
+  const onDecrease = () => {
+    //액션만들고 디스패치
+    dispatch(decrease());
+  };
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+}
+
+export default CounterContainer;
---
+++ src/modules/counter.js
@@ -0,0 +1,23 @@
+// 액션타입선언
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+//액션생성함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+//초기상태
+//꼭 객체이거나 배열일 필요는 없다.
+const intialState = 0;
+
+//state는 intialState 이고, 액션을 받아오겠다.
+export default function counter(state = intialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
